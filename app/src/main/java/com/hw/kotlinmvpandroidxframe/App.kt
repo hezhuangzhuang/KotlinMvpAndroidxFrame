1ab897e4a37f58485f19ff2a102c946704acd666
hezhuangzhuang <790171314@qq.com>
2020-02-03 13:58:38 +0800

    上传新项目

--- app/src/main/java/com/hw/kotlinmvpandroidxframe/App.kt
+++ app/src/main/java/com/hw/kotlinmvpandroidxframe/App.kt
@@ -26,7 +26,6 @@ class App : BaseApp() {
     override fun onCreate() {
         super.onCreate()
 
-
         //腾讯bug线上搜集工具
         CrashReport.initCrashReport(applicationContext, "9455fd103f", false)
 
